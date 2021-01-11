e6dabcfa3c37c28e6490f1db1c7f50cde1a21371
junjie020 <kitchen.gz.020@gmail.com>
2021-01-11 10:11:05 +0800

    remove conan and use c++17 filesystem and optional

--- src/utils/File_Utils.hpp
+++ src/utils/File_Utils.hpp
@@ -12,8 +12,10 @@
 #include <string>
 #include <vector>
 
-#include <boost/filesystem.hpp>
-#include <boost/optional.hpp>
+//#include <boost/filesystem.hpp>
+//#include <boost/optional.hpp>
+#include <filesystem>
+#include <optional>
 
 namespace FileUtils {
 
@@ -34,37 +36,37 @@ bool CopyFile(
     bool createPath = false);
 
 inline std::string GetAbsolutePath(const std::string& filePath) {
-  return boost::filesystem::absolute(filePath).string();
+  return std::filesystem::absolute(filePath).string();
 }
 
 inline std::string GetCurrentFolder() {
-  return boost::filesystem::current_path().string();
+  return std::filesystem::current_path().string();
 }
 
 inline bool FileExists(const std::string& filePath) {
-  return boost::filesystem::exists(filePath) && boost::filesystem::is_regular_file(filePath);
+  return std::filesystem::exists(filePath) && std::filesystem::is_regular_file(filePath);
 }
 
 inline bool FolderExists(const std::string& folderPath) {
-  return boost::filesystem::exists(folderPath) && boost::filesystem::is_directory(folderPath);
+  return std::filesystem::exists(folderPath) && std::filesystem::is_directory(folderPath);
 }
 
 inline std::string getFolder(const std::string& path) {
-  return boost::filesystem::path(path).parent_path().string();
+  return std::filesystem::path(path).parent_path().string();
 }
 
 inline std::string GetFileName(const std::string& path) {
-  return boost::filesystem::path(path).filename().string();
+  return std::filesystem::path(path).filename().string();
 }
 
 inline std::string GetFileBase(const std::string& path) {
-  return boost::filesystem::path(path).stem().string();
+  return std::filesystem::path(path).stem().string();
 }
 
-inline boost::optional<std::string> GetFileSuffix(const std::string& path) {
-  const auto& extension = boost::filesystem::path(path).extension();
+inline std::optional<std::string> GetFileSuffix(const std::string& path) {
+  const auto& extension = std::filesystem::path(path).extension();
   if (extension.empty()) {
-    return boost::none;
+    return nullptr;
   }
   return extension.string().substr(1);
 }
